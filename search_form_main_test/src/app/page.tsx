300d5b47f532cfb7338c3439436a5b69c2251699
jahon <damaboev.jf@gmail.com>
2025-01-27 12:17:12 +0500

    merged test and edu repos from intern

---
+++ search_form_main_test/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { FormEvent } from 'react'
+import Image from 'next/image'
+import SearchPlace from './components/SearchPlace/SearchPlace'
+export default function Home() {
+
+	return (
+		
+		<SearchPlace/>
+
+	)
+}
+
